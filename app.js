0862f1195bff1e7e43d29b3ac8b649ea5040f1f2
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-27 15:20:15 -0700

    feat: set up sessions

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-var express = require('express'),
+var session = require('express-session'),
+	cookieParser = require('cookie-parser'),
+	dbConnection = require('./config/db'),
+	SequelizeStore = require('connect-session-sequelize')(session.Store),
+	express = require('express'),
 	authMiddleware = require('./middleware/authMiddleware'),
 	guestController = require('./controllers/guestController'),
 	adminController = require('./controllers/adminController'),
 	models = require('./models');
 
 var app = express();
+
+//if not logged in keep active for 14 days else 30 minutes
+app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+  	db: dbConnection,
+  	table: 'session'
+  })
+}));
+
+//SequelizeStore;
 
 var port = process.env.PORT || 3000;
 
@@ -17,3 +35,4 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.listen(port);
+
