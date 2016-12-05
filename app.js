33a1e882e2dc6ac28fbfeb6adbf04791da3e6fba
Adam McMahon <mcmahonempire@gmail.com>
2016-12-04 18:38:55 -0700

    feat: set up login

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ var session = require('express-session'),
 	dbConnection = require('./config/db'), // Sequelize
 	SequelizeStore = require('connect-session-sequelize')(session.Store),
 	express = require('express'),
-	authMiddleware = require('./middleware/authMiddleware'),
 	guestController = require('./controllers/guestController'),
 	adminController = require('./controllers/adminController'),
 	questionController = require('./controllers/questionController');
+	choiceController = require('./controllers/choiceController');
 	//csrf token library
+
 var app = express();
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+	extended: true
 }));
 
 
@@ -26,22 +27,21 @@ var twoWeeks = 1000 * 60 * 60 * 24 * 14;
 var thirtyMinutes = 1000 * 60 * 30;
 
 app.use(session({
-  secret: 'keyboard cat',
-  store: new SequelizeStore({
-  	db: dbConnection,
-  	table: 'session',
-  	checkExpirationInterval: thirtyMinutes,
-  	expiration: twoWeeks
-  })
+	secret: 'keyboard cat',
+	store: new SequelizeStore({
+		db: dbConnection,
+		table: 'session',
+		checkExpirationInterval: thirtyMinutes,
+		expiration: twoWeeks
+	})
 }));
 
 var port = process.env.PORT || 3000;
 
-authMiddleware(app);
-
 guestController(app);
 adminController(app);
 questionController(app);
+choiceController(app);
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
