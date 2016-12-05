494f17c4e635960754e1550231626df2a70b62fd
Adam McMahon <mcmahonempire@gmail.com>
2016-12-04 19:05:14 -0700

    refactor: use partials in templates

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var session = require('express-session'),
 
 var app = express();
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json({ type: 'application/*+json' }));       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	extended: true
 }));
